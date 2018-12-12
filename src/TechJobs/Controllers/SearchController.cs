c1b2ab6fb708a1566752fdef0a20209e61e320e6
adiputra.ismail <adiputra.ismail@gmail.com>
2018-12-12 14:49:19 -0600

    Finished 4 of 5 Bonus Missions.

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,39 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            string check = searchType; //char.ToUpper(searchType[0]) + searchType.Substring(1);
+            ViewBag.check = check;
+            ViewBag.term = searchTerm;
+            List<Dictionary<string, string>> jobs;
+            if (searchType.Equals("all"))
+            {
+                if (string.IsNullOrEmpty(searchTerm))
+                {
+                    jobs = JobData.FindAll();
+                }
+                else
+                {
+                    jobs = JobData.FindByValue(searchTerm);
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(searchTerm))
+                {
+                    jobs = JobData.FindAll();
+                }
+                else
+                {
+                    jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                }
+            }
+            ViewBag.jobs = jobs;
+            return View();
+        }
 
     }
 }
